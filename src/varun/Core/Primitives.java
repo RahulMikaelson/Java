7a0f76d26d1a82e1cc86f2d89b47739e588484a5
RahulMikaelson <mikaelsonrahul@gmail.com>
2024-08-07 23:45:51 +0530

    Add Java Primitive Data

---
+++ src/varun/Core/Primitives.java
@@ -0,0 +1,30 @@
+package varun.Core;
+
+public class Primitives {
+    //Integral Numbers
+        // byte
+        // short
+        // int
+        // long
+    //Decimal Numbers
+        // float
+        // double
+    //Characters
+    //Booleans
+    public static void main(String[] args) {
+        // byte data type that store only integers but upto a specific range
+        System.out.println("byte stores values from "+Byte.MIN_VALUE+ " to "+Byte.MAX_VALUE);
+        // short data type that store only integers but upto a specific range
+        System.out.println("short stores values from "+Short.MIN_VALUE+ " to "+Short.MAX_VALUE);
+        // int data type that store only integers but upto a specific range
+        System.out.println("int stores values from "+Integer.MIN_VALUE+ " to "+Integer.MAX_VALUE);
+        // long data type that store only integers but upto a specific range
+        System.out.println("long stores values from "+Long.MIN_VALUE+ " to "+Long.MAX_VALUE);
+        // float data type that store only integers but upto a specific range
+        System.out.println("float stores values from "+Float.MIN_VALUE+ " to "+Float.MAX_VALUE);
+        // double data type that store only integers but upto a specific range
+        System.out.println("long stores values from "+Double.MIN_VALUE+ " to "+Double.MAX_VALUE);
+
+
+    }
+}
